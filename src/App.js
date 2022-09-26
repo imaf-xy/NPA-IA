4d7ddedaf3d982c407b18c0f5360d64139f94491
imaf-xy <igor.matias@nave.org.br>
2022-09-25 23:31:21 -0300

    capa quase pronta .txt

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Head from "./Head";
-import Footer from "./Footer";
-import Body from "./Body";
+import Head from "./Head/Estrutura.jsx";
+import Footer from "./Footer/Estrutura.jsx";
+import Body from "./Body/Estrutura.jsx";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         <Head></Head>
     </div>
   );
 }
